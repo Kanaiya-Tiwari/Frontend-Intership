950eaa17a3a06daf9e92bdf1ea154d0038fdd01b
Kanaiya Tiwari <kanaiya.tiwari@1Rintern.local>
2023-02-06 01:10:15 -0800

    JSON COMMIT

--- JSONTASK/asset/json.js
+++ JSONTASK/asset/json.js
@@ -17,17 +17,46 @@ const data={
   
   // TASK 1
 
+
+  // FIRST WAY
   const stringData=JSON.stringify(data);
   console.log(stringData)
+
+  // SECOND WAY
+
+  const data2=`{
+    "browsers": {
+      "firefox": {
+        "name": "Firefox",
+        "pref_url": "about:config",
+        "releases": {
+          "1": {
+            "release_date": "2004-11-09",
+            "status": "retired",
+            "engine": "Gecko",
+            "engine_version": "1.7"
+          }
+        }
+      }
+    }
+  }`
+  
+
+
+
 
 
   // TASK 2
 
+
+  // FIRST WAY
    const objectdata=JSON.parse(stringData);
    console.log(objectdata)
 
+// Second way
 
-
+const objectdata2=JSON.parse(data2);
+console.log(objectdata2)
 
 
    //TASK 3
